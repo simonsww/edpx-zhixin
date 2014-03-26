898623cdce7a086d8797de68359834f274e9158d
moonreplace <moonreplace@gmail.com>
2014-03-27 00:20:41 +0800

    增添对于绝对路径的替换

--- test/proxyLinks-spec.js
+++ test/proxyLinks-spec.js
@@ -4,9 +4,17 @@ describe('Test the proxy links function', function () {
     var proxyUrl = 'http://www.baidu.com/';
     
     it('should can replace all A tag links with http start', function (done) {
-        var content = '<a href="http://www.163.com">'; 
+        var content = '<a href="http://www.163.com" ></a> <a href="http://www.sina.com"></a>'; 
         var proxyedContent = proxy.proxyLinks(content, proxyUrl);  
-        expect(proxyedContent).toContain(proxyUrl+"?url=http://www.163.com");
+        expect(proxyedContent).toContain(proxyUrl + "?url=http://www.163.com");
+        expect(proxyedContent).toContain(proxyUrl + "?url=http://www.sina.com");
         done(); 
     }); 
+
+    it('should can replace all absolute links with proxy', function (done) {
+        var content = '<a href="/s.html" ></a> <a href="http://www.sina.com"></a>';
+        var proxyedContent = proxy.proxyLinks(content, proxyUrl);
+        expect(proxyedContent).toContain(proxyUrl + "?url=http://www.baidu.com/s.html");
+        done();
+    });
 });
