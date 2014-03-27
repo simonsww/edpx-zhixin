e80da7b239bb9ad17312a5696dd8e6560b1dab75
moonreplace <moonreplace@gmail.com>
2014-03-27 23:17:51 +0800

    添加了对图片url的替换

--- test/proxyLinks-spec.js
+++ test/proxyLinks-spec.js
@@ -17,4 +17,13 @@ describe('Test the proxy links function', function () {
         expect(proxyedContent).toContain(proxyUrl + "?url=http://www.baidu.com/s.html");
         done();
     });
+
+    it('should can replace all image url with proxy', function (done) {
+        var content = 'just for test <img src="http://bcs.baidu.com/weigou-prodb-image/6d5b7d0fd8695f9f3d64a06e8ffa7337_0_600x600.jpg" alt="test"/>  <img src="http://t10.baidu.com/it/u=3064510521,2747029512&fm=58" alt="huawei" />';
+        var proxyedContent = proxy.proxyLinks(content, proxyUrl);
+        expect(proxyedContent).toContain(proxyUrl + '?url=http://bcs.baidu.com/weigou-prodb-image/6d5b7d0fd8695f9f3d64a06e8ffa7337_0_600x600.jpg');
+        expect(proxyedContent).toContain(proxyUrl + '?url=http://t10.baidu.com/it/u=3064510521,2747029512&fm=58');
+        done();
+
+    });
 });
