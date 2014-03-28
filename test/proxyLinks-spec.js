fc5937e57dd9cfd5e5d4f90613da07b6d6380c66
moonreplace <moonreplace@gmail.com>
2014-03-28 19:47:27 +0800

    修复了不能显示图片的问题，及只替换图片

--- test/proxyLinks-spec.js
+++ test/proxyLinks-spec.js
@@ -3,7 +3,7 @@ var proxy = require('../lib/proxyLinks');
 describe('Test the proxy links function', function () {
     var proxyUrl = 'http://www.baidu.com/';
     
-    it('should can replace all A tag links with http start', function (done) {
+    /*it('should can replace all A tag links with http start', function (done) {
         var content = '<a href="http://www.163.com" ></a> <a href="http://www.sina.com"></a>'; 
         var proxyedContent = proxy.proxyLinks(content, proxyUrl);  
         expect(proxyedContent).toContain(proxyUrl + "?url=http://www.163.com");
@@ -16,7 +16,7 @@ describe('Test the proxy links function', function () {
         var proxyedContent = proxy.proxyLinks(content, proxyUrl);
         expect(proxyedContent).toContain(proxyUrl + "?url=http://www.baidu.com/s.html");
         done();
-    });
+    });*/
 
     it('should can replace all image url with proxy', function (done) {
         var content = 'just for test <img src="http://bcs.baidu.com/weigou-prodb-image/6d5b7d0fd8695f9f3d64a06e8ffa7337_0_600x600.jpg" alt="test"/>  <img src="http://t10.baidu.com/it/u=3064510521,2747029512&fm=58" alt="huawei" />';
